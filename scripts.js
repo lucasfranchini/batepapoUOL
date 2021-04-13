e5d6a2c464fab12fd4ed375968a6651665f9a747
Lucas Franchini <lucasfranchini.a@gmail.com>
2021-04-13 02:10:48 -0300

    criação da funçãod e seleção de membros e de visibilidade

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 function mostrarOuEsconderTela(tela){
  document.querySelector(`.${tela}`).classList.toggle("escondido");
 }
+function selecionarMembros(membro){
+    const lista = membro.parentNode.children;
+    for(let i=0;i<lista.length;i++){
+        lista[i].children[1].classList.add("escondido");
+    }
+    membro.children[1].classList.remove("escondido");
+}
