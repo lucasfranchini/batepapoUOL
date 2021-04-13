a6b59961c036a4226cbaa8a26ea380056d7d8297
Lucas Franchini <lucasfranchini.a@gmail.com>
2021-04-13 01:58:10 -0300

    mudando o nome e concatenando duas funções

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-function mostrar(itemescondido){
- document.querySelector(`.${itemescondido}`).classList.remove("escondido");
-}
-function esconder(tela){
-    tela.parentNode.classList.add("escondido");
+function mostrarOuEsconderTela(tela){
+ document.querySelector(`.${tela}`).classList.toggle("escondido");
 }
